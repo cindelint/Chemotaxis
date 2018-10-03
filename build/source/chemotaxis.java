c62b6a9a1ccbc427be78328f2415ba7555da94de
cindelint <cindylee776@gmail.com>
2018-10-03 10:28:12 -0700

    created base bacteria class and draw function, no creative stuff yet

--- build/source/chemotaxis.java
+++ build/source/chemotaxis.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class chemotaxis extends PApplet {
+
 //declare bacteria variables here
 Bacteria[] bob;
-void setup() {
-  size(300,300);
+public void setup() {
+  
 
   bob = new Bacteria[50];
   for (int i=0; i<bob.length; i++) {
@@ -10,7 +26,7 @@ void setup() {
   //initialize bacteria variables here
 }
 
-void draw() {
+public void draw() {
   background(200);
   for (int i=0; i<bob.length; i++) {
     bob[i].show();
@@ -26,7 +42,7 @@ class Bacteria {
     myX = (float) Math.random() * 300;
     myY = (float) Math.random() * 300;
   }
-  void move() {
+  public void move() {
     if (mouseX > myX) {
       myX += (float) (Math.random() * 7) - 2;
     } else {
@@ -39,8 +55,18 @@ class Bacteria {
     }
 
   }
-  void show() {
+  public void show() {
     fill(100);
     ellipse(myX, myY, 20, 20);
+  }
+}
+  public void settings() {  size(300,300); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
