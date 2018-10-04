7ca43912ccfa2d3ed46f2dc6a610db74d3bd4726
cindelint <cindylee776@gmail.com>
2018-10-04 10:24:34 -0700

    added the rip message

--- Chemotaxis.pde
+++ Chemotaxis.pde
@@ -1,13 +1,18 @@
 //declare bacteria variables here
 Bacteria[] bob;
-void setup() {
-  size(300,300);
+int xPos = 250, yPos = 250;
+String[] message = new String[] {"your apush hw", "college", "be a well rounded person", "avoid being obsessed with college", "more college", "feeling inadequate", "parents", "some homework", "that one project due tomorrow that you havent started"};
 
-  bob = new Bacteria[50];
+
+void setup() {
+  size(500,500);
+
+  bob = new Bacteria[75];
   for (int i=0; i<bob.length; i++) {
     bob[i] = new Bacteria();
   }
-  //initialize bacteria variables here
+
+  textAlign(CENTER, CENTER);
 }
 
 void draw() {
@@ -15,24 +20,33 @@ void draw() {
   for (int i=0; i<bob.length; i++) {
     bob[i].show();
     bob[i].move();
+    if (abs(bob[i].myX - xPos) < 3 && abs(bob[i].myY - yPos) < 3) {
+      background(255,0,0);
+      fill(0);
+      textSize(50);
+      text("rip", 250, 250);
+    }
   }
-  //move and show the bacteria
+
+  person();
 }
 
 class Bacteria {
   //lots of java!
   float myX, myY;
+  int myMessage;
   Bacteria() {
-    myX = (float) Math.random() * 300;
-    myY = (float) Math.random() * 300;
+    myX = (float) Math.random() * width;
+    myY = (float) Math.random() * height;
+    myMessage = (int) (Math.random() * message.length);
   }
   void move() {
-    if (mouseX > myX) {
+    if (xPos > myX) {
       myX += (float) (Math.random() * 7) - 2;
     } else {
       myX += (float) (Math.random() * 7) - 4;
     }
-    if (mouseY > myY) {
+    if (yPos > myY) {
       myY += (float) (Math.random() * 7) - 2;
     } else {
       myY += (float) (Math.random() * 7) - 4;
@@ -41,6 +55,31 @@ class Bacteria {
   }
   void show() {
     fill(100);
-    ellipse(myX, myY, 20, 20);
+    ellipse(myX, myY, 15,15);
+    fill(0);
+    textSize(9);
+    text(message[myMessage], myX, myY);
   }
+}
+
+
+void person() {
+
+  if (keyPressed) {
+    if (keyCode == LEFT && xPos>0) {
+      xPos-=5;
+    } else if (keyCode == RIGHT && xPos<width) {
+      xPos+=5;
+    }
+    if (keyCode == UP && yPos>0) {
+      yPos-=5;
+    } else if (keyCode == DOWN && yPos<height) {
+      yPos+=5;
+    }
+  }
+  fill(255,0,0);
+  ellipse(xPos, yPos, 20, 20);
+  stroke(0);
+  ellipse(xPos-4, yPos-2, 1, 1);
+  ellipse(xPos+4, yPos-2, 1, 1);
 }
--- build/source/chemotaxis.java
+++ build/source/chemotaxis.java
@@ -16,14 +16,19 @@ public class chemotaxis extends PApplet {
 
 //declare bacteria variables here
 Bacteria[] bob;
+int xPos = 250, yPos = 250;
+String[] message = new String[] {"your apush hw", "college", "be a well rounded person", "avoid being obsessed with college", "more college", "feeling inadequate", "parents", "some homework", "that one project due tomorrow that you havent started"};
+
+
 public void setup() {
   
 
-  bob = new Bacteria[50];
+  bob = new Bacteria[75];
   for (int i=0; i<bob.length; i++) {
     bob[i] = new Bacteria();
   }
-  //initialize bacteria variables here
+
+  textAlign(CENTER, CENTER);
 }
 
 public void draw() {
@@ -31,24 +36,33 @@ public void draw() {
   for (int i=0; i<bob.length; i++) {
     bob[i].show();
     bob[i].move();
+    if (abs(bob[i].myX - xPos) < 3 && abs(bob[i].myY - yPos) < 3) {
+      background(255,0,0);
+      fill(0);
+      textSize(50);
+      text("rip", 250, 250);
+    }
   }
-  //move and show the bacteria
+
+  person();
 }
 
 class Bacteria {
   //lots of java!
   float myX, myY;
+  int myMessage;
   Bacteria() {
-    myX = (float) Math.random() * 300;
-    myY = (float) Math.random() * 300;
+    myX = (float) Math.random() * width;
+    myY = (float) Math.random() * height;
+    myMessage = (int) (Math.random() * message.length);
   }
   public void move() {
-    if (mouseX > myX) {
+    if (xPos > myX) {
       myX += (float) (Math.random() * 7) - 2;
     } else {
       myX += (float) (Math.random() * 7) - 4;
     }
-    if (mouseY > myY) {
+    if (yPos > myY) {
       myY += (float) (Math.random() * 7) - 2;
     } else {
       myY += (float) (Math.random() * 7) - 4;
@@ -57,10 +71,35 @@ class Bacteria {
   }
   public void show() {
     fill(100);
-    ellipse(myX, myY, 20, 20);
+    ellipse(myX, myY, 15,15);
+    fill(0);
+    textSize(9);
+    text(message[myMessage], myX, myY);
   }
 }
-  public void settings() {  size(300,300); }
+
+
+public void person() {
+
+  if (keyPressed) {
+    if (keyCode == LEFT && xPos>0) {
+      xPos-=5;
+    } else if (keyCode == RIGHT && xPos<width) {
+      xPos+=5;
+    }
+    if (keyCode == UP && yPos>0) {
+      yPos-=5;
+    } else if (keyCode == DOWN && yPos<height) {
+      yPos+=5;
+    }
+  }
+  fill(255,0,0);
+  ellipse(xPos, yPos, 20, 20);
+  stroke(0);
+  ellipse(xPos-4, yPos-2, 1, 1);
+  ellipse(xPos+4, yPos-2, 1, 1);
+}
+  public void settings() {  size(500,500); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "chemotaxis" };
     if (passedArgs != null) {
